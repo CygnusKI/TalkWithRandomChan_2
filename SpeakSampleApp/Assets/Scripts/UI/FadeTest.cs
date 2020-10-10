c819006b2bf44e2f69db15bc48105f93f78a9735
CygnusKI <iwakou1105@gmail.com>
2020-10-11 05:11:09 +0900

    Scene Movement

---
+++ SpeakSampleApp/Assets/Scripts/UI/FadeTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTest : MonoBehaviour
+{
+	[SerializeField]
+	Fade fade = null;
+
+	public void OnClick()
+	{
+		fade.FadeIn (1, () =>
+		{
+			fade.FadeOut(1);
+		});
+	}
+}
