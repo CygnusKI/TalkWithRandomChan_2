a648cf6ec7fffc2e731fc007027609cde3023667
CygnusKI <iwakou1105@gmail.com>
2020-10-11 12:39:33 +0900

    Add Jumping
    RandomChan is jumping when she input question and answer.

--- SpeakSampleApp/Assets/Scripts/UI/LearnButton.cs
+++ SpeakSampleApp/Assets/Scripts/UI/LearnButton.cs
@@ -10,11 +10,17 @@ public class LearnButton : MonoBehaviour
     public Text text;
     public InputField inputQuestion;
     public InputField inputAnswer;
+    Animator anim;
+    //以下らんだむちゃんオブジェクトにアタッチされているanimatorを参照するための宣言
+    GameObject randomchan_object;
+
 
     void Start()
     {
         inputQuestion = inputQuestion.GetComponent<InputField> ();
         inputAnswer = inputAnswer.GetComponent<InputField> ();
+        randomchan_object = GameObject.Find ("Randomchan");
+        anim = randomchan_object.GetComponent<Animator>();
     }
 
     public void OnClick()
@@ -23,6 +29,7 @@ public class LearnButton : MonoBehaviour
         sw.WriteLine(inputQuestion.text + "***" + inputAnswer.text);
         sw.Flush();
         sw.Close();
+        anim.SetTrigger("In");
         text.text = "保存されました";
     }
 }
